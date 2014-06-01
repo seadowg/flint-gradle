85646181956e555886e76580399dd10cf4f6940f
Callum Stott <callum@seadowg.com>
2014-06-02 00:36:11 +0100

    Puns are cool

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -5,7 +5,7 @@ import static spark.Spark.*;
 public class App {
   public static void main(String[] args) {
     get("/", (req, res) -> {
-      return "Hello, world!";
+      return "Ignited...";
     });
   }
 }
