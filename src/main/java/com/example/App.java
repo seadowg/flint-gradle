370c2927a5ec02a4bd515aa357c58bf475d2d37b
Callum Stott <callum@seadowg.com>
2014-06-02 00:42:51 +0100

    Code golf loves lambdas :neckbeard:

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,8 +4,6 @@ import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
-    get("/", (req, res) -> {
-      return "Ignited...";
-    });
+    get("/", (req, res) -> "Ignited...");
   }
 }
